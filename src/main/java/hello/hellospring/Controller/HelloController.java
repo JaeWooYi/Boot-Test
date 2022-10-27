25eb357a50c648d5273b10c1fb2dae12254a2fde
JaeWooYi <wodn0101@gmail.com>
2022-10-27 20:23:15 +0900

    속상해

--- src/main/java/hello/hellospring/Controller/HelloController.java
+++ src/main/java/hello/hellospring/Controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,13 @@ public class HelloController {
     public String Hello(Model model){
         model.addAttribute("data", "Spring!!!");
         return "hello";     // hello.html인건 이제 말안해도 알지
+    }
+
+    @RequestMapping("/helloMvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+
+        model.addAttribute("name", name);
+
+        return "index1.html";
     }
 }
